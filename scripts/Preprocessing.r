cbdc8dcb302613e3b70f329c2199fea5a86c8b12
Kinten Le <kinten108101@protonmail.com>
2024-05-23 21:39:45 +0700

    Restore unit + convert to Mhz

--- scripts/Preprocessing.r
+++ scripts/Preprocessing.r
@@ -15,12 +15,20 @@ for (i in seq(ncol(data))) {
     min <- Inf
     for (j in seq(nrow(col))) {
         cell <- data[j,i]
+        unit <- ""
         if (typeof(cell) != "double") {
             if (typeof(cell) == "character") {
                 cell <- str_split_1(cell, " ")
                 value <- as.double(cell[1])
                 unit <- cell[2]
                 cell <- value
+                result <- unit == "GHz"
+                if (!is.na(result)) {
+                    if (result) {
+                        cell <- value * 1000
+                        unit <- "MHz"
+                    }
+                }
             } else {
                 cell <- as.double(cell)
             }
@@ -28,7 +36,7 @@ for (i in seq(ncol(data))) {
         if (is.na(cell)) next
         if (cell > max) max <- cell
         if (cell < min) min <- cell
-        data[j,i] <- as.double(cell)
+        data[j,i] <- paste(cell, unit, sep=" ")
     }
     median <- (max + min) / 2
     for (j in seq(nrow(col))) {
