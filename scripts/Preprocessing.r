31b4ee96016f7d32be0e2f83a087613218c99bf7
Kinten Le <kinten108101@protonmail.com>
2024-05-23 20:35:23 +0700

    Use median imputation

--- scripts/Preprocessing.r
+++ scripts/Preprocessing.r
@@ -1,32 +1,42 @@
-# Load necessary library
-library(dplyr)
-
-# Importing data
-intel_cpu <- read.csv("../data/Intel_CPUs.csv")
+data <- read.csv("data/Intel_CPUs.csv")
 
 # Specify the desired columns
 desired_cols <- c("Processor_Base_Frequency", "nb_of_Cores", "nb_of_Threads", "TDP", "Lithography")
 
+library(dplyr)
+
 # Subset the data to only include the desired columns
-intel_cpu_subset <- intel_cpu %>% select(one_of(desired_cols))
+data <- data %>% select(one_of(desired_cols))
 
-# Remove rows with base frequency measured in MHz
-intel_cpu_subset <- intel_cpu_subset %>%
-    filter(!grepl("MHz", Processor_Base_Frequency))
+library(stringr)
+for (i in seq(ncol(data))) {
+    col <- data[i]
+    max <- -Inf
+    min <- Inf
+    for (j in seq(nrow(col))) {
+        cell <- data[j,i]
+        if (typeof(cell) != "double") {
+            if (typeof(cell) == "character") {
+                cell <- str_split_1(cell, " ")
+                value <- as.double(cell[1])
+                unit <- cell[2]
+                cell <- value
+            } else {
+                cell <- as.double(cell)
+            }
+        }
+        if (is.na(cell)) next
+        if (cell > max) max <- cell
+        if (cell < min) min <- cell
+        data[j,i] <- as.double(cell)
+    }
+    median <- (max + min) / 2
+    for (j in seq(nrow(col))) {
+        cell <- data[j,i]
+        if ((typeof(cell) == "character" && cell == "") || (typeof(cell) == "double" && is.na(cell))) {
+            data[j,i] <- median
+        }
+    }
+}
 
-# Convert necessary columns to appropriate types if they are not
-# Removing 'GHz' and converting Processor_Base_Frequency to numeric
-intel_cpu_subset$Processor_Base_Frequency <- as.numeric(gsub(" GHz", "", intel_cpu_subset$Processor_Base_Frequency))
-
-# Additional conversion to ensure nb_of_Cores, nb_of_Threads, TDP, and Lithography are numeric where applicable
-intel_cpu_subset$nb_of_Cores <- as.numeric(intel_cpu_subset$nb_of_Cores)
-intel_cpu_subset$nb_of_Threads <- as.numeric(intel_cpu_subset$nb_of_Threads)
-intel_cpu_subset$TDP <- as.numeric(gsub(" W", "", intel_cpu_subset$TDP))
-intel_cpu_subset$Lithography <- as.numeric(gsub(" nm", "", intel_cpu_subset$Lithography))
-
-# Remove rows with missing values in the specific columns
-cols_to_check <- c("Processor_Base_Frequency", "nb_of_Cores", "nb_of_Threads", "TDP", "Lithography")
-intel_cpu_subset <- intel_cpu_subset[complete.cases(intel_cpu_subset[, cols_to_check]), ]
-
-# Display the resulting subset
-print(intel_cpu_subset)
+print(data)
